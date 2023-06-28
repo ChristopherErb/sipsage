b4123120b28d3a1a4f542d1e3406deaa82716011
Christopher Erb <77641908+ChristopherErb@users.noreply.github.com>
2023-06-28 15:22:16 -0400

    Update Drinks.jsx

--- SipSage/src/components/Drinks.jsx
+++ SipSage/src/components/Drinks.jsx
@@ -1,11 +1,8 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
-
 const Drinks = () => {
-
 const [drinks, setdrinks] = useState(null)
-
 useEffect(()=> {
     const getdrinks = async () => {
     const response = await axios.get('www.thecocktaildb.com/api/json/v1/1')
@@ -15,19 +12,16 @@ useEffect(()=> {
     }
     getdrinks()
 }, [])
-
 let navigate = useNavigate()
-
 const showDrinks = (drink) => {
     navigate(`${drink.idDrink}`)
 }
-
     return drinks ? (
         <div className="drinksListPage">
             <h2>List of drinks</h2>
             {
                 drinks.map((drink) => (
-                    <div className="card" onClick={() => showDrinks(drink)} key={drink.idDrink}>
+                    <div className="drinkCard" onClick={() => showDrinks(drink)} key={drink.idDrink}>
                         <h3>{drink.strDrink}</h3>
                         <img src={drink.strDrinkThumb} alt={drink.strDrink} />
                     </div>
@@ -36,5 +30,4 @@ const showDrinks = (drink) => {
         </div>
     ) : (<h3>Getting thirsty... </h3>)
 }
-
 export default Drinks
