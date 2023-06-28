d0e13c8bdb9c3e64505a559ddcbbd29d9dc6a72c
Christopher Erb <christophererb01@gmail.com>
2023-06-28 13:39:01 -0400

    SIGH

--- SipSage/src/components/Contact.jsx
+++ SipSage/src/components/Contact.jsx
@@ -2,17 +2,36 @@ const Contact = () => {
 
 
 
-    return(
-        <div className="ContactMain">Here's stuff
-            <div className="ContactHead">
-                <h2>We would love to hear from you! </h2>
-            </div>
-        
-        
-        
-        
-        
-        </div>
-    )
+
+const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(formState)
+    setFormState(initialState)
 }
-export default Contact
+
+
+
+  return (
+    <div className="ContactMain">
+      <div className="ContactHead">
+        <h2>We would love to hear from you!</h2>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="TeamMember">Team Member</label>
+        <select id="memberSelection">
+          <option value="contctRa">Ra</option>
+          <option value="contactShei">Sheikh</option>
+          <option value="contactEthan">Ethan</option>
+          <option value="contactChris">Chris</option>
+        </select>
+        <label htmlFor="subject">Subject:</label>
+        <input type="text" id="subject" />
+        <label htmlFor="message">Message</label>
+        <textarea id="message" cols="30" rows="10"></textarea>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+};
+export default Contact;
