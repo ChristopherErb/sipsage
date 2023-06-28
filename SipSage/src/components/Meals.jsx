3aa9c2f44e3e836058bed7b826dc141a116ad05c
Christopher Erb <christophererb01@gmail.com>
2023-06-28 16:38:15 -0400

    MEALS

--- SipSage/src/components/Meals.jsx
+++ SipSage/src/components/Meals.jsx
@@ -5,28 +5,31 @@ import { useNavigate } from 'react-router-dom'
 
 
 const Meals = () => {
-const [meal, setMeal] = useState(null)
 
+const [meals, setMeals] = useState(null)
+let navigate = useNavigate()
 
 useEffect(()=> {
-    const getmeal = async () => {
+    const getmeals = async () => {
+
     const response = await axios.get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef')
-    setMeal(response.data.meal)
+    setMeals(response.data.meals)
     console.log(response)
-    console.log(meal)
     }
-    getmeal()
+    getmeals()
 }, [])
-let navigate = useNavigate()
-const showMeal = (meal) => {
+
+
+
+const showMeals = (meal) => {
     navigate(`${meal.idMeal}`)
 }
-    return meal ? (
+    return meals ? (
         <div className="mealListPage">
             <h2>List of meals</h2>
             {
-                meal.map((meal) => (
-                    <div className="mealCard" onClick={() => showMeal(drink)} key={drink.idMeal}>
+                meals.map((meal) => (
+                    <div className="mealCard" onClick={() => showMeals(meal)} key={meal.idMeal}>
                         <h3>{meal.strMeal}</h3>
                         <img src={meal.strMealThumb} alt={meal.strMeal} />
                     </div>
